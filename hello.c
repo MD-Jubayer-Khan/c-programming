b3e53837bdbb703048dca6d6e68907984940141b
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2023-10-04 13:13:00 +0600

    continue statement wiht for loop

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello I'm getting start with c");
+    int a = 0, i;
+    for (i = 0;i < 5; i++)
+    {
+        continue;
+        a++;
+    }
+    printf("%d %d",i,a);
+    return 0;
 }
